1b98e57aa3902d8f5425e6d11bd58e1afa6c8bd5
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-09 15:06:53 +0000

    Ex 11 Complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
-require_relative './lib/bike'
+require './lib/bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike_name)
+    @bike = bike_name
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,26 @@
 require "./lib/docking_station"
+require "./lib/bike"
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-  #when we instantiate DockingStation class and run release_bike, we expect to get an instance of Bike class
-  station = DockingStation.new
-  bike = station.release_bike
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike.working?)
   end
+
+  it 'docks a bike' do
+    is_expected.to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'docks a bike named bmx' do
+    expect(subject.dock_bike('bmx')).to eq 'bmx'
+  end
+
+  it 'shows the bike name' do
+    expect(subject.bike).to eq @bike
+  end
+
+
 
 end
